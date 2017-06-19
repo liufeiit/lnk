27ce9fada8e483ae67dbb24539da2912483a4d48
liufeiit <liufei_it@126.com>
2017-06-19 17:37:17 +0800

    zookeeper lookup

--- lnk-lookup/src/main/java/io/lnk/lookup/zookeeper/ZooKeeperRegistry.java
+++ lnk-lookup/src/main/java/io/lnk/lookup/zookeeper/ZooKeeperRegistry.java
@@ -57,6 +57,42 @@ public class ZooKeeperRegistry implements Registry {
         return addrList.toArray(new Address[addrList.size()]);
     }
 
+    @Override
+    public void registry(String serviceGroup, String serviceId, String version, int protocol, Address addr) {
+        String path = null;
+        try {
+            path = this.createPath(serviceGroup, serviceId, version, protocol);
+            NotifyMessage message = new NotifyMessage();
+            message.setPath(path);
+            message.setData(StringUtils.EMPTY);
+            message.setMessageMode(MessageMode.PERSISTENT);
+            this.provider.push(message);
+            String server = addr.toString();
+            path += ("/" + server);
+            message.setPath(path);
+            message.setData(server);
+            message.setMessageMode(MessageMode.EPHEMERAL);
+            this.provider.push(message);
+            this.registerHandler(serviceGroup, serviceId, version, protocol, addr);
+            log.info("registry path : {} success.", path);
+        } catch (Throwable e) {
+            log.error("registry path : " + path + " Error.", e);
+        }
+    }
+
+    @Override
+    public void unregistry(String serviceGroup, String serviceId, String version, int protocol) {
+        String path = null;
+        try {
+            path = this.createPath(serviceGroup, serviceId, version, protocol);
+            this.provider.delete(path);
+            this.provider.unregister(path, NotifyHandler.NULL);
+            log.warn("unregistry path : {} success.", path);
+        } catch (Throwable e) {
+            log.error("unregistry path : " + path + " Error.", e);
+        }
+    }
+
     private Set<String> getServerList(String path) {
         Set<String> serverList = this.registryServices.get(path);
         if (serverList == null) {
@@ -132,43 +168,7 @@ public class ZooKeeperRegistry implements Registry {
         }
     }
 
-    @Override
-    public void registry(String serviceGroup, String serviceId, String version, int protocol, Address addr) {
-        String path = null;
-        try {
-            path = this.createPath(serviceGroup, serviceId, version, protocol);
-            NotifyMessage message = new NotifyMessage();
-            message.setPath(path);
-            message.setData(StringUtils.EMPTY);
-            message.setMessageMode(MessageMode.PERSISTENT);
-            this.provider.push(message);
-            String server = addr.toString();
-            path += ("/" + server);
-            message.setPath(path);
-            message.setData(server);
-            message.setMessageMode(MessageMode.EPHEMERAL);
-            this.provider.push(message);
-            this.registerHandler(serviceGroup, serviceId, version, protocol, addr);
-            log.info("registry path : {} success.", path);
-        } catch (Throwable e) {
-            log.error("registry path : " + path + " Error.", e);
-        }
-    }
-
-    @Override
-    public void unregistry(String serviceGroup, String serviceId, String version, int protocol) {
-        String path = null;
-        try {
-            path = this.createPath(serviceGroup, serviceId, version, protocol);
-            this.provider.delete(path);
-            this.provider.unregister(path, NotifyHandler.NULL);
-            log.info("unregistry path : {} success.", path);
-        } catch (Throwable e) {
-            log.error("unregistry path : " + path + " Error.", e);
-        }
-    }
-
-    protected String createPath(String serviceGroup, String serviceId, String version, int protocol) {
+    private String createPath(String serviceGroup, String serviceId, String version, int protocol) {
         StringBuilder sb = new StringBuilder("/").append(ROOT_NODE).append("/");
         sb.append(serviceGroup).append("/").append(serviceId).append("/").append(version).append("/").append(protocol).append("/").append(SERVERS_NODE);
         return sb.toString();
